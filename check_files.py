4ffe75b42ae4a6a2efc490ffd55b109168b5d7db
Michael Robinson <merobi@gmail.com>
2022-06-14 13:25:29 -0400

    first commit
    Signed-off-by: Michael Robinson <merobi@gmail.com>

---
+++ check_files.py
@@ -0,0 +1,46 @@
+def check_files():
+
+    import os
+    import sys
+
+    file_type = str(sys.argv[1])
+    author = str(sys.argv[2])
+    print(f'Checking all .{file_type} files...')
+
+    paths = []
+    for (root, dirs, files) in os.walk('.', topdown=True):
+        for f in files:
+            if f == 'check_files.py':
+                continue
+            elif len(file_type) == 4:
+                if f[-4:] == file_type:
+                    path = os.path.join(root, f)
+                    with open(path) as t:
+                        if f'Copyright 2018-2022 contributors to the {author} project' in t.read():
+                            continue
+                        else:    
+                            paths.append(path)
+            elif len(file_type) == 3:
+                if f[-3:] == file_type:
+                    path = os.path.join(root, f)
+                    with open(path) as t:
+                        if f'Copyright 2018-2022 contributors to the {author} project' in t.read():
+                            continue
+                        else:    
+                            paths.append(path)    
+            elif len(file_type) == 2:
+                if f[-2:] == file_type:
+                    path = os.path.join(root, f)
+                    with open(path) as t:
+                        if f'Copyright 2018-2022 contributors to the {author} project' in t.read():
+                            continue
+                        else:
+                            paths.append(path)            
+
+    num_files = len(paths)
+    print(f'{num_files} files without copyright found.')
+    print(paths)
+
+check_files()
+
+
